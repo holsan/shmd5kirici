74a98653de972914edcd8694e950cc051f568760
☾★ Savunan Hacker <30604035+shcoderman@users.noreply.github.com>
2017-08-25 01:36:33 +0300

    Update shmd5cracker.py

--- shmd5cracker.py
+++ shmd5cracker.py
@@ -10,31 +10,33 @@ import urllib
 import re
 
 def menu():
-        os.system("clear")
-        print(" --- Coded By SavunanHacker \n --- Versiyon : 1.0 \n --- MD5 Online , Brute Force Cracker \n ")
-        print "1 --> MD5 Kırıcı"
-        print "2 --> Çıkış \n"
-        sec = input("İşlem Seçiniz :")
+	print """
 
-        if sec == 1:
-                kirici()
-        elif sec == 2:
-            print("\n Çıkış Yaptınız..\n")
-            sys.exit()
-def kirici():
-        os.system("clear")
-        print "\n 1 --> MD5 Online Kırıcı\n"
-        print "\n 2 --> MD5 Brute Force \n"
-        sec = input("\n İşlem Seçiniz :")
+
+███████╗██╗  ██╗
+██╔════╝██║  ██║
+███████╗███████║
+╚════██║██╔══██║
+███████║██║  ██║
+╚══════╝╚═╝  ╚═╝       [*] MD5 Şifre Kırıcı
+                       [*] Kodlayan : SavunanHacker
+                       [*] Versiyon 1.0                           
+                 
+"""
+        print "[1] Online"
+        print "[2] Brute Force"
+        print "[3] Çıkış \n"
+        sec = input("@sh> ")
 
         if sec == 1:
                 onlinekirici()
         elif sec == 2:
-                bruteforce()
-
+            bruteforce()
+        elif sec == 3:
+        	print "\n\t[-] Çıkış Yaptınız..\n"
+        	sys.exit()
 def onlinekirici():
-        os.system("clear")
-        string=raw_input("\n MD5 Şifrenizi Yazınız : ")
+        string=raw_input("\n @md5şifre> ")
         site = 'http://md5decryption.com/'
         sitecek = urllib.urlencode({'hash':string,'submit':'Decrypt+It!'})
         req = urllib2.Request(site)
@@ -42,25 +44,26 @@ def onlinekirici():
           veri = urllib2.urlopen(req, sitecek)
           data = veri.read()
           match = re.search(r'(Decrypted Text: </b>)(.+[^>])(</font><br/><center>)', data)
-          if match: print '\n --> Şifrenin Kırıldığı Site : %s \n --> Şifre : %s \n' % (site,match.group(2))
-          else: print ' \n --> Şifrenin Kırılamadığı Site : %s \n --> Şifre : Bulunamadı !\n' % site
-        except urllib2.URLError: print ' --> Site : %s \t\t\t --> Çökmüş Olabilir !' % site
+          if match: 
+          	print '\n [+] Şifre : %s' % (match.group(2))
+          else: 
+          	print '\n [-] Şifre : Bulunamadı !\n'
+        except urllib2.URLError: print '\t[-] Site Çökmüş Olabilir !' % site
         raw_input("\n Menüye Dönmek İçin [Enter]'a Basınız.")
         menu()
 
 
 def bruteforce():
-        os.system("clear")
         counter = 0
         lines = 0
-        string=raw_input("\n MD5 Şifrenizi Yazınız : ")
-        wordList = raw_input("\n Wordlist Seçiniz : ")
+        string=raw_input("\n @md5şifre> ")
+        wordList = raw_input("\n @wordlist> ")
         try:
                 wordlistfile = open(wordList)
                 for line in open(wordList):
                         lines += 1
         except IoError:
-                print'Geçersiz Wordlist !'
+                print "[-] Geçersiz Wordlist !"
                 raw_input("\n Menüye Dönmek İçin [Enter]'a Basınız.")
                 menu()
         else:
@@ -73,10 +76,16 @@ def bruteforce():
                 counter += 1
                 percentage_raw = float(counter) / float(lines) * 100
                 percentage_raw ="%0f" % percentage_raw; percentage = str(percentage_raw) + " " + "%"
-                os.system("clear")
                 print(percentage)
                 if wordlistdecrypted == string:
-                        print'\n --> Şifre Kırıldı ! - %s' % line
+                        print'\n [+] Şifre Kırıldı : %s' % line
                         menudon = raw_input("\nMenüye Dönmek İçin [Enter]'a Basınız.")
                         menu()
+if __name__ == "__main__":
+	try:
+		menu()
+	except KeyboardInterrupt as err:
+		print "\n\t[-] Çıkış Yaptınız.."
+		sys.exit(0)
+
 menu()
